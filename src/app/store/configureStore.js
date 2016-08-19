3aa53128af811ff68e377b98652ff078e8acd568
theblackbolt <roberto.hernandez@clearlink.com>
2016-08-18 22:16:28 -0600

    Created sample application to showcase redux-devTools and Module hot reloading

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,7 +1,5 @@
-/*eslint-disable */
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./configureStore.prod.js');
+    module.exports = require('./configureStore.prod.js'); // eslint-disable-line global-require
 } else {
-    module.exports = require('./configureStore.dev.js');
+    module.exports = require('./configureStore.dev.js'); // eslint-disable-line global-require
 }
-/*eslint-enable */
